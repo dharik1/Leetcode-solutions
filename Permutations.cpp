c1ae7ef462295337c1b47aa21e64fceeb5991e25
dharik1 <dharikahamed1@gmail.com>
2017-05-09 14:06:02 +0530

    Create Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> v;
+        sort(nums.begin(),nums.end());
+        perm(nums,0,v);
+        return v;
+    }
+    void perm(vector<int>& nums,int i,vector<vector<int>>& v)
+    {
+        if(i==nums.size()-1)
+        {
+            v.push_back(nums);
+            return;
+        }
+        for(int k=i;k<nums.size();k++)
+        {
+            if(i!=k && nums[i]==nums[k]) 
+            {
+                continue;
+            }
+            swap(nums[i],nums[k]);
+            perm(nums,i+1,v);
+            swap(nums[i],nums[k]);
+        }
+    }
+};
