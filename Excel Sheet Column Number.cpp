31ac6d8f5a8385f7c961f3f6906959ddff928cf7
M J Dharik Ahamed <dharikahamed1@gmail.com>
2017-06-18 16:12:14 +0530

    Create Excel Sheet Column Number.cpp

---
+++ Excel Sheet Column Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int n=0;
+        for(int i=0;i<s.size();i++)
+        {
+            n=n*26+((s[i]-'A')+1);
+        }
+        return n;
+    }
+};
